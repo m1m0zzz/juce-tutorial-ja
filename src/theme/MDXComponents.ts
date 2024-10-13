1a5c822402eb7f49117631e71c62440cd21f853c
m1m0zzz <contact.m1m0zzz@gmail.com>
2024-10-13 16:48:33 +0900

    add: default components

---
+++ src/theme/MDXComponents.ts
@@ -0,0 +1,11 @@
+import MDXComponents from '@theme-original/MDXComponents';
+import CaptionImage from '@site/src/components/CaptionImage';
+import SourcePageLink from '@site/src/components/SourcePageLink';
+import ClassList from '@site/src/components/ClassList';
+
+export default {
+  ...MDXComponents,
+  CaptionImage,
+  SourcePageLink,
+  ClassList,
+};
