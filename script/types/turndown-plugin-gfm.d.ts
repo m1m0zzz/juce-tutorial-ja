1f171b837222c89e9515cfde314c0d6660da907a
m1m0zzz <contact.m1m0zzz@gmail.com>
2025-12-08 06:01:53 +0900

    remove script

--- script/types/turndown-plugin-gfm.d.ts
+++
@@ -1,9 +0,0 @@
-import TurndownService from "turndown";
-
-declare module "turndown-plugin-gfm" {
-  export const gfm: TurndownService.Plugin;
-  export const highlightedCodeBlock: TurndownService.Plugin;
-  export const strikethrough: TurndownService.Plugin;
-  export const tables: TurndownService.Plugin;
-  export const taskListItems: TurndownService.Plugin;
-}
